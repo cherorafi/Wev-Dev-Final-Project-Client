20112961ad0f9e13af71f91166c4e771facc6726
cherorafi <saidun2001@gmail.com>
2023-05-12 03:20:44 -0400

    delete and add functionality

--- src/components/containers/AllCampusesContainer.js
+++ src/components/containers/AllCampusesContainer.js
@@ -7,15 +7,18 @@ If needed, it also defines the component's "connect" function.
 ================================================== */
 import Header from './Header';
 import { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { fetchAllCampusesThunk } from "../../store/thunks";
+
+import { 
+  fetchAllCampusesThunk, 
+  deleteCampusThunk 
+} from "../../store/thunks";
+
 import { AllCampusesView } from "../views";
 
 class AllCampusesContainer extends Component {
   // Get all campuses data from back-end database
   componentDidMount() {
-    console.log(this.props);
     this.props.fetchAllCampuses();
   }
 
@@ -26,9 +29,10 @@ class AllCampusesContainer extends Component {
         <Header />
         <AllCampusesView
           allCampuses={this.props.allCampuses}
+          deleteCampus={this.props.deleteCampus}  
         />
       </div>
-    );
+    )
   }
 }
 
@@ -45,13 +49,8 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchAllCampuses: () => dispatch(fetchAllCampusesThunk()),
+    deleteCampus: (campusId) => dispatch(deleteCampusThunk(campusId)),
   };
-};
-
-// Type check props;
-AllCampusesContainer.propTypes = {
-  allCampuses: PropTypes.array.isRequired,
-  fetchAllCampuses: PropTypes.func.isRequired,
 };
 
 // Export store-connected container by default
--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -8,9 +8,17 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const AllCampusesView = (props) => {
+  const {allCampuses, deleteCampus} = props;
   // If there is no campus, display a message.
-  if (!props.allCampuses.length) {
-    return <div>There are no campuses.</div>;
+  if (!allCampuses.length) {
+    return (
+    <div>
+      <p>There are no campuses.</p>
+      <Link to={`newcampus`}>
+        <button>Add New Student</button>
+      </Link>
+    </div>
+  );
   }
 
   // If there is at least one campus, render All Campuses view 
@@ -18,19 +26,21 @@ const AllCampusesView = (props) => {
     <div>
       <h1>All Campuses</h1>
 
-      {props.allCampuses.map((campus) => (
+      {allCampuses.map((campus) => (
         <div key={campus.id}>
           <Link to={`/campus/${campus.id}`}>
+            
             <h2>{campus.name}</h2>
           </Link>
-          <h4>campus id: {campus.id}</h4>
           <p>{campus.address}</p>
           <p>{campus.description}</p>
+          <button onClick={() => {deleteCampus(campus.id); window.location.reload();}}>Delete</button>
+          <button>Edit</button>
           <hr/>
         </div>
       ))}
       <br/>
-      <Link to={`/`}>
+      <Link to={`newcampus`}>
         <button>Add New Campus</button>
       </Link>
       <br/><br/>
@@ -38,9 +48,5 @@ const AllCampusesView = (props) => {
   );
 };
 
-// Validate data type of the props passed to component.
-AllCampusesView.propTypes = {
-  allCampuses: PropTypes.array.isRequired,
-};
 
 export default AllCampusesView;
